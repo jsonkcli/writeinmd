6bdffe71dacaacc02ca99a12794d97b79be59122
ovatimee <stonegee606@yahoo.com>
2023-06-22 18:07:15 +0000

    testing uploaderzone

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -59,8 +59,8 @@ export function ChatPanel({
   return (
 
     <div>
-      <div className="border-b text-white flex flex-col gap-8 pb-12">
-        <p className="">Write a few sentences about yourself.</p>
+      <div className="border-b text-white flex flex-col gap-8">
+        <p className="">Write a few sentences about your project.</p>
         <div className="">
           <PromptForm
             complete={complete}
@@ -87,30 +87,29 @@ export function ChatPanel({
           <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 ">
             Add Project Demo
           </label>
-          <UploadDropzone
-            uploader={uploader}
-            options={options}
-            onUpdate={files => console.log(files.map(x => x.fileUrl).join("\n"))}
-            onComplete={files => alert(files.map(x => x.fileUrl).join("\n"))}
-            width="600px"
-            height="375px" />
-          {/* <UploadDropZone /> */}
-          {/* <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10"> */}
-          {/*   <div className="text-center"> */}
-          {/*     <Icons.media className="mx-auto h-12 w-12" aria-hidden="true" /> */}
-          {/*     <div className="mt-4 flex text-sm leading-6 "> */}
-          {/*       <label */}
-          {/*         htmlFor="file-upload" */}
-          {/*         className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500" */}
-          {/*       > */}
-          {/*         <span>Upload a file</span> */}
-          {/*         <input id="file-upload" name="file-upload" type="file" className="sr-only" /> */}
-          {/*       </label> */}
-          {/*       <p className="pl-1">or drag and drop</p> */}
-          {/*     </div> */}
-          {/*     <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p> */}
-          {/*   </div> */}
-          {/* </div> */}
+          {/* <UploadDropzone */}
+          {/*   uploader={uploader} */}
+          {/*   options={options} */}
+          {/*   onUpdate={files => console.log(files.map(x => x.fileUrl).join("\n"))} */}
+          {/*   onComplete={files => alert(files.map(x => x.fileUrl).join("\n"))} */}
+          {/*   width="600px" */}
+          {/*   height="375px" /> */}
+          <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+            <div className="text-center">
+              <Icons name="media" className="mx-auto h-12 w-12" aria-hidden="true" />
+              <div className="mt-4 flex text-sm leading-6 ">
+                <label
+                  htmlFor="file-upload"
+                  className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                >
+                  <span>Upload a file</span>
+                  <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                </label>
+                <p className="pl-1">or drag and drop</p>
+              </div>
+              <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+            </div>
+          </div>
         </div>
       </div>
       <div className="mt-6 flex items-center justify-end gap-x-6">
