0eef30b435a8e8fdfa89101ce99fa28e63e5acd1
ovatimee <stonegee606@yahoo.com>
2023-06-26 16:28:19 +0000

    new editor

--- components/editor.tsx
+++ components/editor.tsx
@@ -24,18 +24,18 @@ export default function Editor() {
   });
 
   return (
-    <div className="flex w-full  pt-8 md:px-12 justify-between overflow-y-auto md:border-[0.5px] border-gray-300 rounded-md md:shadow-md shadow-white">
-      <div className=" basis-2/6">
-        <ChatPanel
-          isLoading={isLoading}
-          stop={stop}
-          input={input}
-          setInput={setInput}
-          completion={completion}
-          complete={complete}
+    <div className="flex  pt-8 md:px-12 justify-between overflow-y-auto md:border-[0.5px] border-gray-300 rounded-md md:shadow-md shadow-white">
+      {/* <div className=" basis-2/6"> */}
+      {/*   <ChatPanel */}
+      {/*     isLoading={isLoading} */}
+      {/*     stop={stop} */}
+      {/*     input={input} */}
+      {/*     setInput={setInput} */}
+      {/*     completion={completion} */}
+      {/*     complete={complete} */}
 
-        />
-      </div>
+      {/*   /> */}
+      {/* </div> */}
         <ChatMessage completion={completion} />
     </div>
   )
